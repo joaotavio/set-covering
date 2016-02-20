f5dac70d5c719587957951eb81f6fff377fa3130
joaotavio <joao.o.b@hotmail.com>
2016-02-20 16:37:55 -0200

    Gerar individuo feito

--- src/ag/Cromossomo.java
+++ src/ag/Cromossomo.java
@@ -1,21 +1,26 @@
 package ag;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Cromossomo {
     
     //Lista de indices das colunas que pertencem a solução
-    private ArrayList<Integer> solucao;
+    private ArrayList<Integer> colunas;
     private double custoTotal;
 
     public Cromossomo() {
-        solucao = new ArrayList<>();
+        colunas = new ArrayList<>();
         custoTotal = 0;
     }
     
     public void addColuna(int index, double custo){
-        solucao.add(index);
+        colunas.add(index);
         custoTotal += custo;
+    }
+
+    public ArrayList<Integer> getColunas() {
+        return colunas;
     }
     
     public static void gerarIndividuo(ArrayList<Integer>[] listaLinha, ArrayList<Integer>[] listaColuna, Double[] listaCusto){
@@ -27,31 +32,41 @@ public class Cromossomo {
             linhasDescobertas.add(i);
         }
         
-        int w[] = new int[listaLinha.length];
+        int qtdColunaCobreLinha[] = new int[listaLinha.length];
         Cromossomo cromossomo = new Cromossomo();
         
         while (!linhasDescobertas.isEmpty()){
             int random_pos = Util.getRandomInt(linhasDescobertas.size());
+            int linha = linhasDescobertas.get(random_pos);
             
-            ArrayList<Integer> alfa = listaLinha[random_pos];
-            double custo = listaCusto[alfa.get(0)];
-            int intersecao_size = Util.sizeIntersecao(linhasDescobertas, listaColuna[alfa.get(0)]);
-            double menor = custo / intersecao_size;
-            int menorColuna = 0;
-            for (int i = 1; i < alfa.size(); i++) {
-                custo = listaCusto[alfa.get(i)];
-                intersecao_size = Util.sizeIntersecao(linhasDescobertas, listaColuna[alfa.get(i)]);
-                if ((custo / intersecao_size) < menor){
-                    menor = custo / intersecao_size;
-                    menorColuna = i;
-                }
-            }
+            ArrayList<Integer> conjuntoColuna = listaLinha[linha];
+            int menorColuna = colunaMinimizaCusto(conjuntoColuna, linhasDescobertas, listaColuna, listaCusto);
             
             cromossomo.addColuna(menorColuna, listaCusto[menorColuna]);
-            //wi
-            System.out.println(linhasDescobertas);
-            linhasDescobertas.removeAll(listaColuna[menorColuna]);
-            
+            removerCobertos(qtdColunaCobreLinha, linhasDescobertas, listaColuna[menorColuna]);
+        }
+    }
+    
+    private static int colunaMinimizaCusto(ArrayList<Integer> conjuntoColuna, ArrayList<Integer> linhasDescobertas, ArrayList<Integer>[] listaColuna, Double[] listaCusto){
+        double menor = Double.MAX_VALUE;
+        int menorColuna = -1;
+        for (int i = 0; i < conjuntoColuna.size(); i++) {
+            int coluna = conjuntoColuna.get(i);
+            double custo = listaCusto[coluna];
+            int intersecao_size = Util.sizeIntersecao(linhasDescobertas, listaColuna[coluna]);
+            if ((custo / intersecao_size) < menor) {
+                menor = custo / intersecao_size;
+                menorColuna = coluna;
+            }
+        }
+        return menorColuna;
+    }
+    
+    private static void removerCobertos(int[] qtdColunaCobreLinha, ArrayList<Integer> linhasDescobertas, ArrayList<Integer> conjuntoLinhas) {
+        for (int i = 0; i < conjuntoLinhas.size(); i++) {
+            int linha = conjuntoLinhas.get(i);
+            qtdColunaCobreLinha[linha]++;
+            linhasDescobertas.remove(new Integer(linha));
         }
     }
     
